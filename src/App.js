1e35469c9431199029d385507cce8d2bd839048c
nicolesylvester <niccisylvester@gmail.com>
2023-11-17 18:15:26 -0700

    Initial pages and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// import React
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import other pages
+import CourseEntry from "./CourseEntry";
+import MainMenu from "./MainMenu";
+import SubunitPage from "./SubunitPage.js"; 
+
+// import style sheet
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  return (
+    // define routes to other pages
+    <Router>
+      <Routes> 
+        <Route exact path="/" element={<CourseEntry />} />
+        <Route path="/MainMenu" element={<MainMenu />} />
+        <Route path="/course/:unit/:subunit" element={<SubunitPage />} />
+      </Routes>
+    </Router>
+  )
+}
