515842c8d90653a59178bc5fa47868e5870a4a07
dayraquin <71858583+dayraquin@users.noreply.github.com>
2024-01-03 00:02:00 -0700

    Update SubunitPage.js

--- src/SubunitPage.js
+++ src/SubunitPage.js
@@ -7,8 +7,7 @@ import { useParams } from 'react-router-dom';
 // import other pages
 import './SubunitPage.css';
 import Menu from "./Navigation.js"
-import ThreeDModel from './ThreeDModel.js';
-
+import {ThreeDModel} from './ThreeDModel.js';
 
 export default function SubunitPage() 
 {
@@ -21,11 +20,11 @@ export default function SubunitPage()
   const handleColorChange = (value) => {
     setSelectedColor(value);
   };
-
+ 
   const handleWidthChange = (value) => {
-    setWidth(value);
+    setWidth(value); 
   };
-  
+
   return (
     <div className="subunit-page">
       {/* Hamburger menu */}
@@ -45,8 +44,33 @@ export default function SubunitPage()
           width={width}
           shape={currentShape}
         /></div>
-        
         <div className="controls-container">
+        <div className="toggle-switch">
+          <label>
+            <p  style={{ position:'absolute', top: -50, right: 10}}> Biological</p>
+            <p  style={{ position:'absolute', top: -50, left: 55}}> Sex</p>
+            <span className="label" style={{position:'absolute', bottom: 10, right: 70}}>Male</span>
+            <input
+              type="checkbox"
+              checked={currentShape === 'sphere'}
+              onChange={() => setCurrentShape(currentShape === 'cube' ? 'sphere' : 'cube')}
+            />
+            <span className="slider"></span>
+            <span className="label" style={{position:'absolute', bottom: 10, right: -60}}>Female</span>
+          </label>
+          </div>
+        <div className="control-group">
+            <label htmlFor="width-slider">Body Size:</label>
+            <input
+              id="width-slider"
+              type="range"
+              min="1"
+              max="5"
+              step="0.1"
+              value={width}
+              onChange={(e) => handleWidthChange(parseFloat(e.target.value))}
+            />
+          </div>
           <div className="control-group">
             <label htmlFor="color-slider">Skin Tone:</label>
             <input
@@ -59,35 +83,12 @@ export default function SubunitPage()
               onChange={(e) => handleColorChange(parseFloat(e.target.value))}
             />
           </div>
-          <div className="control-group">
-            <label htmlFor="width-slider">Width:</label>
-            <input
-              id="width-slider"
-              type="range"
-              min="1"
-              max="5"
-              step="0.1"
-              value={width}
-              onChange={(e) => handleWidthChange(parseFloat(e.target.value))}
-            />
-          </div>
-          <div className="toggle-switch">
-          <label>
-            <span className="label">Cube</span>
-            <input
-              type="checkbox"
-              checked={currentShape === 'sphere'}
-              onChange={() => setCurrentShape(currentShape === 'cube' ? 'sphere' : 'cube')}
-            />
-            <span className="slider"></span>
-            <span className="label">Sphere</span>
-          </label>
+          {/*TODO: render content dynamically*/}
           <div className="content-container">
             <p> Content: </p> <br></br>
             <p> The side of a 3D shape is the face.</p> <br></br>
             <p> The edges of a 3D shape are where the faces meet</p>
           </div>
-        </div>
       </div>
     </div>
   </div>
