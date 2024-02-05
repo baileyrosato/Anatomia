fd5189a040d83c72000e9e941a4b3f38123367e4
nicolesylvester <niccisylvester@gmail.com>
2024-02-04 20:35:15 -0700

    added tooltip for camera

--- src/ModelRendering/Interface.jsx
+++ src/ModelRendering/Interface.jsx
@@ -1,13 +1,28 @@
 import { Affix, Button, Group } from "@mantine/core";
 import { useCharacterCustomization } from "./CharacterCustomizationContext.jsx";
 import { SettingsConfigurator } from "./SettingsConfigurator.jsx";
+import {Tooltip} from "@mui/material/";
+import {QuestionMark} from '@mui/icons-material';
 
 import "./Interface.css"; // import style sheet
+import { IconButton} from "@mui/material";
 
 const Interface = () => {
   const { headConfiguratorOpen, setHeadConfiguratorOpen } = useCharacterCustomization();
   return (
     <div className="model-scene-container">
+      <Tooltip title={
+    <span>
+      Use mouse or touch gestures to interact with the model. <br />
+      Zoom: scroll <br />
+      Rotate: left click and drag <br />
+      Pan: right click and drag
+    </span>
+  } placement="bottom">
+        <IconButton>
+          <QuestionMark/>
+        </IconButton>
+      </Tooltip>
       <Affix position={{ top: 180, left: 50 }}>
         <Group>
           <Button
