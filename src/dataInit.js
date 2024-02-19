bbed0a78f7a5d55989485f961419d0a8c553f877
nicolesylvester <niccisylvester@gmail.com>
2024-02-19 10:32:06 -0700

    initial db

--- src/dataInit.js
+++ src/dataInit.js
@@ -11,23 +11,28 @@ const courseData = [
     subunits: [  
       {
         title:'Anatomical Position', 
-        description: "In clinical settings, it is important to have a standard frame of reference. Anatomical position refers to the starting point and is a reference point that ensures everyone is speaking about the same side or part of the body. Anatomical position is always in reference to the patient. In anatomical position, the patient's body is upright, facing forward, arms straight and down at the patient's side with palms facing forward, legs straight, feet flat on the ground, and eyes open.\n\nThinking Question: Do you think the model shown is in anatomical position? Why or why not?"
+        description: "In clinical settings, it is important to have a standard frame of reference. Anatomical position refers to the starting point and is a reference point that ensures everyone is speaking about the same side or part of the body. Anatomical position is always in reference to the patient. In anatomical position, the patient's body is upright, facing forward, arms straight and down at the patient's side with palms facing forward, legs straight, feet flat on the ground, and eyes open.\n\nThinking Question: Do you think the model shown is in anatomical position? Why or why not?",
+        image: '' 
       },
       {
         title:'Directional Terms', 
-        description: 'Remember to consider anatomical position when referencing directional terms. Here is a list of directional terms and their definitions. Superior: Above, Inferior: Below, Medial: Towards the midline of the body, Lateral: away from the midline of the body, Superficial: Towards the surface, Deep: Towards the core of the body, Anterior: To the front, Posterior: To the back, Proximal: for extremities, near the trunk, Distal: for extremities, away from the trunk. These terms are used to compare body regions in relation to one another. Example: The eyes are superior to the mouth. Thinking Question: Referring to the model, can you come up with more examples using directional terms?'
+        description: 'Remember to consider anatomical position when referencing directional terms. Here is a list of directional terms and their definitions. Superior: Above, Inferior: Below, Medial: Towards the midline of the body, Lateral: away from the midline of the body, Superficial: Towards the surface, Deep: Towards the core of the body, Anterior: To the front, Posterior: To the back, Proximal: for extremities, near the trunk, Distal: for extremities, away from the trunk. These terms are used to compare body regions in relation to one another. Example: The eyes are superior to the mouth. Thinking Question: Referring to the model, can you come up with more examples using directional terms?',
+        image: '' 
       },
       {
         title:'Planes of Sectioning',
-        description: 'Clinically we now have available to us numerous imaging technologies that “slice” the body into different planes. Depending on the plane of sectioning, you will see a very different image. Therefore, being familiar with these planes of sectioning is important so that you can orient the view. Transverse (horizontal or cross-section) plane: divides the body or the organ into superior and inferior parts. Frontal (coronal) plane: divides the body or the organ into anterior and posterior parts. Sagittal plane: divides the body or organ into right and left parts. Mid-sagittal plane: divides the body into equal right and left halves. Parasagittal plane: divides the body into unequal right and left parts.'
+        description: 'Clinically we now have available to us numerous imaging technologies that “slice” the body into different planes. Depending on the plane of sectioning, you will see a very different image. Therefore, being familiar with these planes of sectioning is important so that you can orient the view. Transverse (horizontal or cross-section) plane: divides the body or the organ into superior and inferior parts. Frontal (coronal) plane: divides the body or the organ into anterior and posterior parts. Sagittal plane: divides the body or organ into right and left parts. Mid-sagittal plane: divides the body into equal right and left halves. Parasagittal plane: divides the body into unequal right and left parts.',
+        image: '' 
       },
       {
         title:'Body Cavities', 
-        description: 'The body contains two main body cavities. These are the ventral and the dorsal cavity. The ventral cavity is further divided into Thoracic cavity, superior to the diaphragm, which is divided into the mediastinum and the pleural(2) and pericardial cavities. Abdominopelvic cavity, inferior to the diaphragm, which is divided into the abdominal and pelvic cavities. The dorsal cavity is further subdivided into Cranial cavity Vertebral cavity'
+        description: 'The body contains two main body cavities. These are the ventral and the dorsal cavity. The ventral cavity is further divided into Thoracic cavity, superior to the diaphragm, which is divided into the mediastinum and the pleural(2) and pericardial cavities. Abdominopelvic cavity, inferior to the diaphragm, which is divided into the abdominal and pelvic cavities. The dorsal cavity is further subdivided into Cranial cavity Vertebral cavity',
+        image: '' 
       },
       {
         title:'Regions of the Body', 
-        description: 'The abdomen is divided into either four quadrants or 9 regions.'
+        description: 'The abdomen is divided into either four quadrants or 9 regions.',
+        image: './images/RegionsOfBody.jpeg' 
       }
     ]
   },
