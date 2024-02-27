31116455517760889ce5402d835f53af102f9627
nicolesylvester <niccisylvester@gmail.com>
2024-02-27 13:34:12 -0700

    whiteboard fix

--- src/ModelRendering/Whiteboard.js
+++ src/ModelRendering/Whiteboard.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Tldraw } from '@tldraw/tldraw';
 
 const Whiteboard = ({ imageData }) => {
+
+  // const components = {
+  //   Background: imageData,
+  // }
+  
+
   return (
     <div className="whiteboard-container" style={{ width: '80%', height: '80%', margin: "auto", position: 'fixed', inset: 0 }}>
-      <Tldraw backgroundImage={imageData} />
+      <Tldraw />
     </div>
   );
 };
