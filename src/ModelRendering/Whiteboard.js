9c12ac223c42257312339920b62eb4ee4acf1502
nicolesylvester <niccisylvester@gmail.com>
2024-03-13 13:43:37 -0700

    removed debug panel from whiteboard

--- src/ModelRendering/Whiteboard.js
+++ src/ModelRendering/Whiteboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Tldraw } from '@tldraw/tldraw';
+import './Whiteboard.css';
+
+const components ={
+  DebugMenu: null,
+}
 
 const Whiteboard = ({ imageData }) => {
     return (
     <div className="whiteboard-container" >
-      <Tldraw />
+      <Tldraw components={components}/>
     </div>
   );
 };
